eabd9e0714826f3a16496738d51214bb84e1c0e4
RoboMarchello <faccclol123@gmail.com>
2026-07-17 11:26:56 +0300

    Added simple saving system. Proud of it:)

--- experiments/drawing_prototype.py
+++ experiments/drawing_prototype.py
@@ -2,12 +2,21 @@
 import sys
 sys.path.append('/src')
 
+import os
+
+def file_exists(filename):
+    try:
+        os.stat(filename)
+        return True
+    except OSError:
+        return False
+
 from lib.color_setup import screen
 from machine import Pin
 import utime
 import struct
 from drivers.rotary_encoder import RotaryEncoder
-
+    
 # config
 SCREEN_SIZE = SCREEN_W, SCREEN_H = 480, 320
 
@@ -15,6 +24,8 @@ ENCODER_L_CLK, ENCODER_L_DT = 12, 13
 ENCODER_R_CLK, ENCODER_R_DT = 18, 19
 
 BRUSH_RADIUS = 3
+
+CHUNK_SIZE = 512
 
 # define colors to be used
 COLORS_SETUP = [
@@ -43,7 +54,20 @@ encoder_right = RotaryEncoder(
 )
 
 def main():
-    screen.fill(COLORS['WHITE']) 
+    # inject save file
+    if file_exists('save.bin'):
+        with open('save.bin', 'rb') as file:
+            while True:
+                chunk = file.read(CHUNK_SIZE)
+                if not chunk:
+                    break
+
+                binary = bytearray(chunk)
+                pos = file.tell()
+                screen.mvb[pos-CHUNK_SIZE:pos] = binary
+    else:
+        screen.fill(COLORS['WHITE']) 
+    
     screen.show()
 
     etcher_pos = [0, 0]
@@ -74,5 +98,11 @@ def main():
             screen.ellipse(etcher_pos[0], etcher_pos[1], BRUSH_RADIUS, BRUSH_RADIUS, COLORS['RED'], False)
             screen.show_region(etcher_pos[0]-BRUSH_RADIUS, etcher_pos[1] - BRUSH_RADIUS, BRUSH_RADIUS*2+3, BRUSH_RADIUS*2+3)
 
-
-main()
+try:
+    main()
+except Exception as e:
+    print(e)
+finally:
+    # write save file
+    with open('save.bin', 'wb') as file:
+        file.write(screen.mvb)
